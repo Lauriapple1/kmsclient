eaa4c1c4322adca8a705decebbab9bc358e7268e
Mark Kelly <mkelly@zalando.ie>
2015-08-13 14:57:46 +0200

    added pypi necessary files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'kmsclient',
+    packages = ['kmsclient'],
+    version = '0.1',
+    description = 'For encrypting and decrypting using a specific key from amazons kms',
+    author = 'Mark Kelly',
+    author_email = 'mkelly28@tcd.ie',
+    url = 'https://github.com/zalando/kmsclient',
+    download_url = 'https://github.com/zalando/kmsclient/tarball/0.1',
+    keywords = ['kms', 'aws', 'key', 'management','system', 'client','encryption'],
+    classifiers = [],
+)
