9345f0ec8faf86573832c4d85e00399f4c62511c
Mark Kelly <mkelly@zalando.ie>
2015-08-13 15:06:34 +0200

    fixup setup.py

---
+++ kmsclient/__main__.py
@@ -0,0 +1,4 @@
+import kmsclient.cli
+
+if __name__ == '__main__':
+    kmsclient.cli.main()
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
 setup(
-    name = 'kmsclient',
-    packages = ['kmsclient'],
-    version = '0.1',
-    description = 'For encrypting and decrypting using a specific key from amazons kms',
-    author = 'Mark Kelly',
-    author_email = 'mkelly28@tcd.ie',
-    url = 'https://github.com/zalando/kmsclient',
-    download_url = 'https://github.com/zalando/kmsclient/tarball/0.1',
-    keywords = ['kms', 'aws', 'key', 'management','system', 'client','encryption'],
-    classifiers = [],
+    name='kmsclient',
+    packages=['kmsclient'],
+    version='0.1',
+    description='For encrypting and decrypting using a specific key from amazons kms',
+    author='Mark Kelly',
+    author_email='mkelly28@tcd.ie',
+    url='https://github.com/zalando/kmsclient',
+    download_url='https://github.com/zalando/kmsclient/tarball/0.1',
+    keywords=['kms', 'aws', 'key', 'management', 'system', 'client', 'encryption'],
+    classifiers=[],
+    entry_points={'console_scripts': 'kmsclient = kmsclient.cli:main'},
+    install_requires=['clickclick' >= 0.10, 'pyperclip', 'boto3']
 )
